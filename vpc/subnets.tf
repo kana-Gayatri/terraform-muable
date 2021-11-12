8fe5fe8d8fdb5192169e48a848984fbf2c4487f0
Gayatri <gayatrikanaparthi@gmail.com>
2021-11-13 00:04:35 +0500

    region changed to us-east-2

---
+++ vpc/subnets.tf
@@ -0,0 +1,12 @@
+variable "SUBNETS" {
+  default = ""
+}
+resource "aws_subnet" "subnets" {
+count             = length(var.SUBNETS)
+vpc_id            = aws_vpc.main.id
+cidr_block        = element(var.SUBNETS, count.index)
+
+tags = {
+  Name = "subnet-${count.index}"
+}
+}

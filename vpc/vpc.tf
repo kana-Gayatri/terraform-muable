e4f3dceae3c28820a42e3bc1980c5d973c7246b8
Gayatri <gayatrikanaparthi@gmail.com>
2021-11-15 19:17:08 +0500

    changes done in subnets.tf

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,15 +1,27 @@
-#output "ENV" {
-#  value =  var.ENV
+#variable "VPC_CIDR" {
+#  default = ""
+#}
+resource  "aws_vpc"   "subnets" {
+  count      = length(var.SUBNETS)
+  cidr_block       =  element(var.SUBNETS,count.index )
+  tags = {
+    Name ="subnet.${count.index}"
+      }
+}
+
+#variable "ENV" {}
+
+
+#resource "aws_vpc" "main" {
+#  cidr_block = var.VPC_CIDR_MAIN
+#
+#  tags = {
+#    Name = var.ENV
+#  }
 #}
 
-variable "VPC_CIDR" {
-  default = ""
-}
-resource  "aws_vpc"   "main" {
-  cidr_block       =  var.VPC_CIDR
-  tags = {
-    Name = var.ENV
-  }
-}
-
-variable "ENV" {}
+#resource "aws_vpc_ipv4_cidr_block_association" "addon" {
+#  count      = length(var.VPC_CIDR_ADDON)
+#  vpc_id     = aws_vpc.main.id
+#  cidr_block = element(var.VPC_CIDR_ADDON, count.index)
+#}

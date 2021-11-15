89e8c23aa40be4f029a0cb0f80ab635b7e22f137
Gayatri <gayatrikanaparthi@gmail.com>
2021-11-15 22:27:37 +0500

    changes done in subnets.tf

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -1,14 +1,14 @@
-resource "aws_route_table" "route" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "example"
-  }
+#resource "aws_route_table" "route" {
+#  vpc_id = aws_vpc.main.id
+#  tags = {
+#    Name = "example"
+#  }
 #vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 #resource "aws_route" "r" {
 #  route_table_id            = aws_route_table.route.id
 #  destination_cidr_block    = var.DEFAULT_VPC_CIDR
 #  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 #}
-}
+#}
 
 
--- vpc/vars.tf
+++ vpc/vars.tf
@@ -1,6 +1,6 @@
 variable "ENV" {}
 variable "VPC_CIDR" {}
-#variable "SUBNETS" {}
+variable "SUBNETS" {}
 #variable "VPC_CIDR_MAIN" {}
 #variable "VPC_CIDR_ADDON" {}
 

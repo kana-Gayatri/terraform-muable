8907522c96927358d1ff880f661c468ef6448a23
Gayatri <gayatrikanaparthi@gmail.com>
2021-11-15 19:35:07 +0500

    changes done in subnets.tf

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -3,7 +3,8 @@ variable "SUBNETS" {
 }
 resource "aws_subnet" "subnets" {
 count             = length(var.SUBNETS)
-vpc_id            = aws_vpc.main.id
+#vpc_id            = aws_vpc.main.id
+  vpc_id            = aws_vpc.main[count.index]
 cidr_block        = element(var.SUBNETS, count.index)
 tags = {
   Name = "subnet-${count.index}"

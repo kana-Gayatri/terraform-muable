3f4f107b60bb990180aaa11fae823311924c4a65
Gayatri <gayatrikanaparthi@gmail.com>
2021-11-15 20:23:07 +0500

    changes done in subnets.tf

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "subnets" {
   #  depends_on        = [aws_vpc_ipv4_cidr_block_association.addon]
   count      = length(var.SUBNETS)
   vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.SUBNETS, count.index)
+  cidr_block =   element(var.SUBNETS, count.index)
   #  availability_zone = element(var.AZS, count.index)
 
   tags = {

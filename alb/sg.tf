525bd0ac4ad93484e1c1597b1e4837cfc3c39d6e
Gayatri <gayatrikanaparthi@gmail.com>
2021-11-24 23:14:22 +0500

    created loadbalancer

--- alb/sg.tf
+++ alb/sg.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "public-alb" {
       from_port        = 80
       to_port          = 80
       protocol         = "tcp"
-      cidr_blocks      = "0.0.0.0/0"
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
       security_groups  = []

8c377b2bccf9e9ed618fd607b14292940a2fbe29
Gayatri <gayatrikanaparthi@gmail.com>
2021-12-06 22:28:54 +0530

    commented records in route53.tf to chech the app-module to cart repo

--- app-module/lb.tf
+++ app-module/lb.tf
@@ -32,23 +32,23 @@ resource "aws_lb_target_group_attachment" "tg-attach" {
 #
 #}
 #
-#resource "aws_lb_listener_rule" "private" {
-#  count        = var.IS_PRIVATE_LB ? 1 : 0
-#  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
-#  priority     = var.LB_RULE_PRIORITY
-#
-#  action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.tg.arn
-#  }
-#
-#  condition {
-#    host_header {
-#      values = ["${var.COMPONENT}-${var.ENV}.roboshop.internal"]
-#    }
-#  }
-#}
-#
+resource "aws_lb_listener_rule" "private" {
+  count        = var.IS_PRIVATE_LB ? 1 : 0
+  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
+  priority     = var.LB_RULE_PRIORITY
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.COMPONENT}-${var.ENV}.roboshop.internal"]
+    }
+  }
+}
+
 #resource "aws_lb_listener" "public-listener" {
 #  count             = var.IS_PRIVATE_LB ? 0 : 1
 #  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN

917bbc922a634399fe7a2b6061d885087dc73e36
Gayatri <gayatrikanaparthi@gmail.com>
2021-12-31 23:57:29 -0800

    script added in ec2 and sg.tf for prometheus

--- app-module/ec2.tf
+++ app-module/ec2.tf
@@ -36,3 +36,10 @@ resource "aws_ec2_tag" "ec2-monitor-tag" {
   key         = "monitor"
   value       = "yes"
 }
+
+resource "aws_ec2_tag" "ec2-env-tag" {
+  count       = length(local.INSTANCE_IDS)
+  resource_id = element(local.INSTANCE_IDS, count.index)
+  key         = "environment"
+  value       = var.ENv
+}
